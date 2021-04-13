a748869a79152be7bb1d57935495b36a29f7a2d7
Bartek Glowacki <marathone@gmail.com>
2021-04-13 19:04:59 +0200

    Return Unknown if unknown format

--- lib/string_data_format.rb
+++ lib/string_data_format.rb
@@ -3,6 +3,7 @@ require "date"
 require_relative "url_params"
 require_relative "date_format"
 require_relative "date_time_format"
+require_relative "unknown"
 
 class StringDataFormat
   def initialize(string)
@@ -14,6 +15,6 @@ class StringDataFormat
     return "uuid" if UUID.validate(@string)
     return "date-time" if DateTimeFormat.validate(@string)
     return "date" if DateFormat.validate(@string)
-    "unknown"
+    Unknown.new.to_s
   end
 end
---
+++ lib/unknown.rb
@@ -0,0 +1,9 @@
+class Unknown
+  def unknown?
+    true
+  end
+
+  def to_s
+    "unknown"
+  end
+end
